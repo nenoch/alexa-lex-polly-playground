0ae285e59a3c67492fda55e6126f59dae6047587
Irene Canuti <irene.canuti@gmail.com>
2017-11-15 21:25:15 +0000

    Create the call of the github api for lex

---
+++ gitHubLex/lex-basic.js
@@ -0,0 +1,31 @@
+'use strict';
+const request = require('request');
+
+exports.handler = (event, context, callback) => {
+    var user = event.currentIntent.AMAZON.Person;
+    callback(null, getUser(User));
+};
+
+function getUser(user) {
+    const baseUrl = `https://api.github.com/users/${user}`;
+    const headers = {
+        'User-Agent': 'something'
+    };
+    const options = {
+        headers: headers
+    };
+    
+    request.get(baseUrl, options, (err, res, body) => {
+        let result = JSON.parse(body);
+        return {
+            dialogAction: {
+                type: 'Close',
+                fulfillmentState: "Fulfilled",
+                message: {
+                    contentType: "PlainText",
+                    content: `I found ${result.login}, ${result.bio}`
+                }
+            }
+        }           
+    });
+}
